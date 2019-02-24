77f3af9e88ce3607796d474d24053db8e1ccbe8c
oshadhAK <oshadhakatulanda.6@cse.mrt.ac.lk>
2019-02-24 08:28:24 +0530

    toolbarupdate

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import {FormControl} from '@angular/forms';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
+  
 })
 export class SearchComponent implements OnInit {
-
-  constructor(private dataService:DataService) { }
+  userID : any;
+  constructor(private dataService:DataService) { 
+    this.userID = "Oshadha";
+  }
 
   ngOnInit() {
   }
