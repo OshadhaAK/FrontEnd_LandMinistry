4f7a12f53f2127107138a557bba95f2ddd70a09f
oshadhAK <oshadhakatulanda.6@cse.mrt.ac.lk>
2019-02-25 20:52:34 +0530

    adminupdate

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -13,7 +13,11 @@ export class AdminComponent implements OnInit {
   userID : any;
   constructor() {
     this.userID = "Oshadha";
-    this.detailSet = [{name:"oshadha",email:"asdav@adv",password:"e32e",telno:54656},{name:"Kalana",email:"dfed@ree",password:"54tr",telno:546436},{name:"Heshan",email:"vcvf@riue",password:"b67r",telno:5576}];
+    this.detailSet = [
+    {name:"oshadha",email:"asdav@adv",password:"e32e",telno:54656},
+    {name:"Kalana",email:"dfed@ree",password:"54tr",telno:546436},
+    {name:"Heshan",email:"vcvf@riue",password:"b67r",telno:5576}
+  ];
     
    }
   
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,11 +3,7 @@ import { DataService } from '../data.service';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
-export interface Project {
-  
-  name: string;
-  
-}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -15,42 +11,14 @@ export interface Project {
   
 })
 export class SearchComponent implements OnInit {
-  projectCtrl = new FormControl();
-  filteredProjects: Observable<Project[]>;
-
-  projects: Project[] = [
-    {
-      name: 'Southern Express',
-      
-    },
-    {
-      name: 'Nothern Express',
-      
-    },
-    {
-      name: 'Eastern Express',
-      
-    },
-    {
-      name: 'Western Express',
-      
-    }
-  ];
+  
 
   userID : any;
   constructor(private dataService:DataService) { 
     this.userID = "Oshadha";
-    this.filteredProjects = this.projectCtrl.valueChanges
-      .pipe(
-        startWith(''),
-        map(project => project ? this._filterProjects(project) : this.projects.slice())
-      );
+    
   }
-  private _filterProjects(value: string): Project[] {
-    const filterValue = value.toLowerCase();
-
-    return this.projects.filter(project => project.name.toLowerCase().indexOf(filterValue) === 0);
-  }
+  
   ngOnInit() {
   }
 
