ed23355b6d83bc5388eedd912821e31d59d03815
oshadhAK <oshadhakatulanda.6@cse.mrt.ac.lk>
2019-02-27 18:40:43 +0530

    projectdetails update

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-detail.component.scss']
 })
 export class ProjectDetailComponent implements OnInit {
-  state = "switch";
+  state = "select";
   constructor() { }
 
   ngOnInit() {
